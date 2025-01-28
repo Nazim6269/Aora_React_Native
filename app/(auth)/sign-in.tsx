0f0a649f24d4b0c9690332ea9208d81f90565b6c
nazim <nazimdev10022001@gmail.com>
2025-01-29 00:06:08 +0600

    added some design with twnrc pacage

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,11 +1,28 @@
-import { View, Text } from "react-native";
+import { Image, ScrollView, Text, View } from "react-native";
 import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { images } from "@/constants";
+import tw from "twrnc";
 
 const SignIn = () => {
   return (
-    <View>
-      <Text>SignIn</Text>
-    </View>
+    <SafeAreaView style={tw`bg-slate-950 h-full`}>
+      <ScrollView>
+        <View style={tw`w-full flex justify-center h-full px-4 my-6`}>
+          <Image
+            source={images.logo}
+            resizeMode="contain"
+            style={tw`w-[115px] h-[34px]`}
+          />
+
+          <Text
+            style={tw`text-2xl font-semibold text-white mt-10 font-psemibold`}
+          >
+            Log in to Aora
+          </Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
